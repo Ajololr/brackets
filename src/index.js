779ce6c170088573ec8f3b54bc56fefe524ac398
Ilya Androsov <ilya_androsov@icloud.com>
2019-10-07 21:34:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let resultMas = [];
+    let map = new Map(bracketsConfig);
+    for (let i = str.length - 1; i >= 0; i--) {
+        if ((map.get(str[i]) === resultMas[0]) && (resultMas[0] !== undefined)) {
+            resultMas.shift();
+        }
+            else {
+                resultMas.unshift(str[i]);
+            }
+    }
+    return resultMas.length === 0; 
+} 
